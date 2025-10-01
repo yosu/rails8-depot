29edbacd4f2747cdfbab754a089e7198c63f8462
yosu <woodstock830@gmail.com>
2025-10-01 18:18:16 +0900

    Add line item highlight test

--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -15,4 +15,12 @@ class CartsTest < ApplicationSystemTestCase
 
     assert has_no_content? "Your Cart"
   end
+
+  test "check highlight line item" do
+    visit store_index_url
+
+    click_on "Add to Cart", match: :first
+
+    assert has_css? "table tr.line-item-highlight"
+  end
 end
