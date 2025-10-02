0e9935896c64d23410ee1227416abf7370ee42c5
yosu <woodstock830@gmail.com>
2025-10-02 17:44:21 +0900

    Add password diferrential validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :email_address, presence: true, uniqueness: true
   has_secure_password
   has_many :sessions, dependent: :destroy
+  validate :password, :password_must_be_different_from_current, if: -> { password_digest_was.present? }
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
@@ -15,6 +16,14 @@ class User < ApplicationRecord
     def ensure_an_admin_remains
       if User.count.zero?
         raise Error.new "Can't delete last user"
+      end
+    end
+
+    # ensure that the password will change
+    def password_must_be_different_from_current
+      if BCrypt::Password.new(password_digest_was) == password
+        errors.add(:base, "Password must be different from current")
+        throw :abort
       end
     end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create user" do
+    user = User.new({
+      name: "dave",
+      email_address: "dave@example.com",
+      password: "secret",
+      password_confirmation: "secret"
+    })
+
+    assert user.save
+  end
+
+  test "update password must be different from current" do
+    user = users(:one)
+
+    assert_not user.update(password: "password", password_confirmation: "password")
+    assert user.update(password: "secret", password_confirmation: "secret")
+  end
 end
